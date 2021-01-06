eacaee69697b98cfe60b1f550c363bb41a4fcec3
kendu <1210870730@qq.com>
2021-01-06 18:39:29 +0800

    基本框架搭建

---
+++ lib/pages/tabs/Home.dart
@@ -0,0 +1,29 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          RaisedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/search');
+            },
+            child: Text("跳转"),
+          ),
+        ],
+      ),
+    );
+  }
+}
